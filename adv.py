d01625836154cbee41fde1348111569022e4ecf9
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-01-24 14:57:41 -0700

    stuck on player movements

--- adv.py
+++ adv.py
@@ -3,9 +3,31 @@ from player import Player
 from world import World
 from util import Stack, Queue #Use this for storage
 
-import random
+import random #do we have a use for this?
 from ast import literal_eval
 
+def bfs(starting_room_id):
+    # Create a queue
+    q = Queue()
+    # Enqueue starting point in a list to start path = starting_room_id
+    q.enqueue([starting_room_id])
+    visited = set() 
+
+    # While there is stuff in the queue
+    while q.size()> 0:
+        # pop/grab the first available path
+        path = q.dequeue()
+        # current_room = last item in the path
+        current_room = path[-1]
+        print("current room", current_room, "\n")
+        # print('Add to visited', visited.add(current_room))
+        # If not visited
+        if current_room not in visited:
+            visited[current_room] = path
+            
+            # For each...edge?...direction in the map graph's current_room
+            for direction in mapDictionary[current_room]:
+                print('MapGraph Directions', mapDictionary[current_room], "\n")
 
 
 # Load world
@@ -40,7 +62,10 @@ mapDictionary = {}
 # vertex = current_room
 # edges = paths - directions?
 
-            
+
+# Try BFS/t --> searching paths to the next room?
+
+
 # DFS/t --> Generating Maze???
 # need to connect/ compare to room_graph? --> ancestors?
 # keep track of previously traveled rooms? counter?
@@ -48,6 +73,7 @@ mapDictionary = {}
 
 """
 Need a dictionary = mapGraph
+room 0 = r0
 {
     room_id      dir : exit?, dir : r5, dir : exit? dir : exit?
 room    0:      {'n' : '?',    's': 5,   'w': '?',   'e': '?'  },
@@ -78,6 +104,7 @@ def search(starting_room):
     #prints out cardinal direction
     # print("Exits", player.current_room.get_exits() ) 
 
+    # Building the graph
     # make a dictionary of rooms with [n,s,e,w] as key:pair = either room_id or '?'
     room_dict ={}
     # conditional or loop
@@ -90,8 +117,10 @@ def search(starting_room):
             for i in current_room.get_exits():
                 # add the "?" in the room dictionary?
                 # print("I", i) # prints n direction
+                # add the key at [i] and the value that = '?'
                 room_dict[i] = '?'
-                print("room dictionary", room_dict[i]) 
+                # print("room dictionary", room_dict[i]) 
+            # updating the room
             if traversal_path:
                 prevRoom = opp_directions[traversal_path[-1]]
                 room_dict[prevRoom] = prevRoom
@@ -99,35 +128,68 @@ def search(starting_room):
             mapDictionary[room_id] = room_dict
 
         else:
+            # do we need to do something other than break the loop?
             break
-
-        # What next?
+        
         # We see there is an unexplored '?'
-        # 
+        # Need to see if a room is connected or not
+        possible_exits = list()
+
+        # print("Room dictionary check", room_dict) # prints the direction and the '?' Can we use this??
+        # iterate through our room dictionary 
+        for direction in room_dict:
+            # print("direction in room", room_dict[direction])
+            # if '?' is at index direction
+            if room_dict[direction] == '?':
+                # What to do?
+                # Store them???
+                possible_exits.append(direction)
+                # print(f"We have a possible exit {possible_exit}")
+        
+        # Can we use the above to discover/ travel to that room?
+        # print(len(possible_exit)) #Gives us a length of 1
+        # If there is an unknown direction....
+        if len(possible_exits) > 0:
+            # print("We need to move to the next room")
+            print(f'{possible_exits[0]} is the next possible direction')
+            # direction = the possible direction[0]
+            direction = possible_exits[0]
+            # print('We moved',direction)
+            
+            # move the player in the direction using travel function
+            player.travel(direction)
+            # we haven't moved the player???
+            print("players current room", room_id)
+            # how do we move them
+        else:
+            # DO SOMETHING!
+            # BFS to search for next exit
+            # If an exit has been explored, you can put it in your BFS queue like normal.
+            print("using bfs",bfs(room_id)) #not running right now
         
 
 print("Search Function", search(room_graph))
 print("------------------")
 print("Map Graph Dictionary", mapDictionary) #{0: {'n': '?'}}
-print("------------------")
+# print("------------------")
 print("Traversal path", traversal_path)
-print("------------------")
+# print("------------------")
 
 
 # TRAVERSAL TEST
-visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
+# visited_rooms = set()
+# player.current_room = world.starting_room
+# visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
+# for move in traversal_path:
+#     player.travel(move)
+#     visited_rooms.add(player.current_room)
 
-if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
-else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+# if len(visited_rooms) == len(room_graph):
+#     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+# else:
+#     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+#     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
 
@@ -144,23 +206,3 @@ else:
 #     else:
 #         print("I did not understand that command.")
 
-
-# def maze(starting_room):
-#     room = starting_room
-#     # Create a stack/queue as appropriate
-#     stack = Stack()
-#     # Put the starting point in that
-#     stack.push(room)
-#     # Make a set to keep track of where we have been
-#     visited = {}
-#     # While there is stuff in the stacl/queue
-#     while stack.size() < len(room_graph):
-#         print("Fisrt Room call", room)
-#         path = stack.pop()
-#         print("Room path", path)
-#         # room = path[-1]
-#         break
-#         # if room not in visited:
-#         #     print("Room if statement", room)
-#         #     # visited[room] = {}
-#         #     # print(visited)
